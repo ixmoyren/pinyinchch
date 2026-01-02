75cfd04748928f42ac86796b7c4589d807ca13a3
ixmoyren <mxonren@foxmail.com>
2026-01-02 21:32:46 +0800

    feat: 为模型创建对应的类型

---
+++ pinyinchch-type/lib.rs
@@ -0,0 +1,67 @@
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "rkyv",
+    derive(rkyv::Archive, rkyv::Deserialize, rkyv::Serialize)
+)]
+pub struct HmmData {
+    pub data: HashMap<String, f64>,
+    pub default: f64,
+}
+
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "rkyv",
+    derive(rkyv::Archive, rkyv::Deserialize, rkyv::Serialize)
+)]
+pub struct HmmPy2HzData {
+    #[cfg_attr(feature = "serde", serde(flatten))]
+    pub data: HashMap<String, String>,
+}
+
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "rkyv",
+    derive(rkyv::Archive, rkyv::Deserialize, rkyv::Serialize)
+)]
+pub struct HmmTransitionData {
+    pub data: HashMap<String, HashMap<String, f64>>,
+    pub default: f64,
+}
+
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "rkyv",
+    derive(rkyv::Archive, rkyv::Deserialize, rkyv::Serialize)
+)]
+pub struct HmmEmissionData {
+    pub data: HashMap<String, HashMap<String, f64>>,
+    pub default: f64,
+}
+
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "rkyv",
+    derive(rkyv::Archive, rkyv::Deserialize, rkyv::Serialize)
+)]
+pub struct DagCharData {
+    #[cfg_attr(feature = "serde", serde(flatten))]
+    pub data: HashMap<String, Vec<(String, f64)>>,
+}
+
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(
+    feature = "rkyv",
+    derive(rkyv::Archive, rkyv::Deserialize, rkyv::Serialize)
+)]
+pub struct DagPhraseData {
+    #[cfg_attr(feature = "serde", serde(flatten))]
+    pub data: HashMap<String, Vec<(String, f64)>>,
+}
