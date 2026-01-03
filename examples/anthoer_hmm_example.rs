1acad57ccb11facb1adf8d8e17aa3507c14f0783
ixmoyren <mxonren@foxmail.com>
2026-01-03 23:54:50 +0800

    chore: 提供一个使用拼音拆分方法和 HMM 转换的示例

---
+++ examples/anthoer_hmm_example.rs
@@ -0,0 +1,31 @@
+use pinyinchch::hmm::{DefaultHmm, viterbi};
+use pinyinchch::pinyin::pinyin_split_by_trie_tokenizer;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // 创建 HMM 实例
+    let hmm = DefaultHmm::default();
+
+    let pinyin = "nihao";
+
+    let pinyin_split_str = pinyin_split_by_trie_tokenizer(pinyin);
+
+    let pinyin_seq = pinyin_split_str.split(" ").collect::<Vec<_>>();
+
+    // 使用 Viterbi 算法，返回 2 个候选结果，不使用对数概率
+    let result = viterbi(&hmm, &pinyin_seq, 2, false, 3.14e-200);
+
+    println!("HMM结果:");
+    for item in &result {
+        println!("分数: {}, 路径: {:?}", item.score(), item.path());
+    }
+
+    // 使用对数概率
+    let result_log = viterbi(&hmm, &pinyin_seq, 2, true, 3.14e-200);
+
+    println!("\nHMM结果 (对数概率):");
+    for item in &result_log {
+        println!("分数: {}, 路径: {:?}", item.score(), item.path());
+    }
+
+    Ok(())
+}
